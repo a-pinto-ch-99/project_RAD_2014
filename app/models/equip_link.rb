7e5195c72ab2adee0b460975e086c24c182c7968
a-pinto-ch-99 <anthony.pinto@net2000.ch>
2014-07-01 14:49:18 +0200

    Active Record configuration in models

--- app/models/equip_link.rb
+++ app/models/equip_link.rb
@@ -1,2 +1,4 @@
 class EquipLink < ActiveRecord::Base
+  belongs_to :equipments
+  belongs_to :links
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ActiveRecord::Base
+  has_many :equip_links
+  has_many :equipments, thourgh: :equip_links 
 end
